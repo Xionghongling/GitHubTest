d31e33859c11237fc4a5ab505dd7f57626125664
xiong <3173047193@qq.com>
2023-06-02 11:07:35 +0800

    这是我提交的第一个数据

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        int[] a={1,2,4,5,6,7};
+        for (int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
